35f07f7af8627e6ae26711df0ae3cb01f064dfa9
Nicholas Beaudoin <nick.s.beaudoin@gmail.com>
2023-06-24 19:49:01 -0700

    Completed gogole search app and updated requirements

--- google_search_app.py
+++ google_search_app.py
@@ -10,8 +10,25 @@ from langchain.utilities import GoogleSearchAPIWrapper
 # load credentials
 load_dotenv()  # take environment variables from .env.
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GOOGLE_CSE_ID = os.getenv("GOOGLE_CSE_ID")
 
 llm = OpenAI(model='text-davinci-003', temperature=0)
 
 search = GoogleSearchAPIWrapper()
 
+tools = [
+    Tool(
+        name='google-search',
+        func=search.run,
+        description='useful for when you need to search google to answer questions about current events'
+    )
+]
+
+agent = initialize_agent(tools,
+                         llm,
+                         agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+                         verbose=True,
+                         max_iterations=6)
+
+response = agent("What's the latest news on the Titan sub?")
+print(response["output"])
