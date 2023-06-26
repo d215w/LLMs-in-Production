94339bc0f8725d9242ecab8197e9b59a1712f079
Nicholas Beaudoin <nick.s.beaudoin@gmail.com>
2023-06-25 17:26:57 -0700

    Completed token app

--- Module 1/exceed_token_limit.py
+++ Module 1/exceed_token_limit.py
@@ -1,2 +1,28 @@
 from langchain.llms import OpenAI
+from dotenv import load_dotenv
+import os
 
+# Load the OpenAI key
+load_dotenv()  # take environment variables from .env.
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# define model
+llm = OpenAI(model="text-davini-003")
+
+# define input text
+input_text = "you_long_input_text"
+
+# determine the max number of tokens from the documentation
+max_tokens = 4097
+
+# split the input text into chunks of tokens based on the max tokens
+text_chunks = split_text_into_chunks(input_text, max_tokens)
+
+# process each chunk separately
+results= []
+for chunk in text_chunks:
+    result = llm.process(chunk)
+    results.append(results)
+
+# combine the resulst as needed
+final_result = combine_results(results)
