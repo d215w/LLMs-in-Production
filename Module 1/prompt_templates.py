cc36aec086378be624d4e5041b81597ff09cd5cf
Nicholas Beaudoin <nick.s.beaudoin@gmail.com>
2023-06-27 08:58:56 -0700

    final commit

--- Module 1/prompt_templates.py
+++ Module 1/prompt_templates.py
@@ -2,6 +2,8 @@ from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 from langchain import PromptTemplate
 from langchain import FewShotPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain import LLMChain
 
 from dotenv import load_dotenv
 import os
@@ -35,4 +37,32 @@ example_prompt = PromptTemplate(
 )
 
 # break prompt into prefix and suffix
-prefix = """The following are excerpts"""
+prefix = """The following are excerpts from conversations with an AI
+assistant. The assistant is known for its humor and wit, providing 
+entertaining and amusing responses to users' questions. Here are some
+examples:
+"""
+
+# the suffix 
+suffix = """
+User: {query}
+AI: """
+
+# now create the few-shot prompt template
+few_shot_prompt_template = FewShotPromptTemplate(
+    examples=examples,
+    example_prompt=example_prompt,
+    prefix=prefix,
+    suffix=suffix,
+    input_variables=["query"],
+    example_separator="\n\n"
+)
+
+# Time to run it!
+
+# Load the model
+chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+
+chain = LLMChain(llm=chat, prompt=few_shot_prompt_template)
+print(chain.run("What's the meaning of life"))
+
