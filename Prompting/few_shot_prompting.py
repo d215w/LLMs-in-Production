81607719fd55853f447924d864343559079293bc
Nicholas Beaudoin <nick.s.beaudoin@gmail.com>
2023-07-09 18:01:59 -0700

    few shot prompting app

---
+++ Prompting/few_shot_prompting.py
@@ -0,0 +1,48 @@
+from langchain import PromptTemplate, FewShotPromptTemplate
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
+import os
+
+# Load the OpenAI key
+load_dotenv()  # take environment variables from .env.
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# initiliaze LLM
+llm = OpenAI(model_name="text-davinci-003", temperature=0)
+
+examples = [
+    {"color": "red", "emotion": "passion"},
+    {"color": "blue", "emotion": "serenity"},
+    {"color": "green", "emotion": "tranquility"},
+]
+
+example_formatter_template = """
+Color: {color}
+Emotion: {emotion}\n"""
+
+example_prompt = PromptTemplate(
+    input_variables=("color", "emotion"),
+                     template=example_formatter_template,
+    )
+
+few_shot_prompt = FewShotPromptTemplate(
+    examples=examples,
+    example_prompt=example_prompt,
+    prefix="Here are some examples of colors and the emotion associated with them:\n\n",
+    suffix="\n\nNow, given a new color, identify the emotion associated with it:\n\nColor: {input}\nEmotion:",
+    input_variables=["input"],
+    example_seperator="\n",
+)
+
+formatted_prompt = few_shot_prompt.format(input="purple")
+
+# create the LLMChain for the prompt
+chain = LLMChain(llm=llm,
+                 prompt=PromptTemplate(template=formatted_prompt,
+                                       input_variables=[]))
+
+# Run the LLMChain to get the new content
+response = chain.run({})
+
+print("Color: purple")
+print("Emotion:", response)
