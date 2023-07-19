7d7b2bbbdb4f42f3a8573326522869fe075228fb
Nicholas Beaudoin <nick.s.beaudoin@gmail.com>
2023-07-18 18:45:05 -0700

    final push today

---
+++ Advanced Prompting/few_shot_prompting_adv.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+import os
+
+from langchain import FewShotPromptTemplate
+
+# create our examples
+examples = [
+    {
+        "query": "What's the weather like?",
+        "answer": "It's raining cats and dogs, better bring an umbrella!"
+    }, {
+        "query": "How old are you?",
+        "answer": "Age is just a number, but I'm timeless."
+    }
+]
+
+# create an example template
+example_template
