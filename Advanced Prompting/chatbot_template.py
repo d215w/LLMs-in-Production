6eadf9fdf0f867ca7c59b509ba5a5968cc94d42a
Nicholas Beaudoin <nick.s.beaudoin@gmail.com>
2023-07-18 18:34:33 -0700

    initial commit chat_template app

---
+++ Advanced Prompting/chatbot_template.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+import os
+
+from langchain.chat_models import ChatOpenAI
+from langchain import LLMChain
+from langchain.prompts.chat import(
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+    
+# Load the OpenAI key
+load_dotenv()  # take environment variables from .env.
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+
+template = "You are a helpful assistant that translated english to pirate"
+system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+example_human = HumanMessagePromptTemplate.from_template("Hi")
+example_ai = AIMessagePromptTemplate.from_template("Argh me mateys")
+human_template="{text}"
+human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, 
+                                                example_human,
+                                                example_ai,
+                                                human_message_prompt])
+chain = LLMChain(llm=chat, prompt=chat_prompt)
+chain.run("I love programming.")
