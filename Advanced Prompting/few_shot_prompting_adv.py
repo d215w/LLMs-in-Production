8d3af798f2010aceec04010a87b82a19e40b2755
Nicholas Beaudoin <nick.s.beaudoin@gmail.com>
2023-07-21 20:27:49 -0700

    bug fix in few shot prompt script

--- Advanced Prompting/few_shot_prompting_adv.py
+++ Advanced Prompting/few_shot_prompting_adv.py
@@ -1,30 +1,14 @@
 from dotenv import load_dotenv
 import os
 
-from langchain import FewShotPromptTemplate
+from langchain import PromptTemplate, FewShotPromptTemplate, LLMChain
 from langchain.chat_models import ChatOpenAI
-from langchain import LLMChain
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    AIMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
 
-# Before executing the following code, make sure to have
-# your OpenAI key saved in the “OPENAI_API_KEY” environment variable.
-chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+# Load the OpenAI key
+load_dotenv()  # take environment variables from .env.
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-template="You are a helpful assistant that translates english to pirate."
-system_message_prompt = SystemMessagePromptTemplate.from_template(template)
-example_human = HumanMessagePromptTemplate.from_template("Hi")
-example_ai = AIMessagePromptTemplate.from_template("Argh me mateys")
-human_template="{text}"
-human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-
-chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, example_human, example_ai, human_message_prompt])
-chain = LLMChain(llm=chat, prompt=chat_prompt)
-chain.run("I love programming.")
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
 
 # create our examples
